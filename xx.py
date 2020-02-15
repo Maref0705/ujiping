be2ebee831a2789e6d2f9e0d5d604725bd3e038c
Maref0705 <60621806+Maref0705@users.noreply.github.com>
2020-02-15 08:41:22 +0700

    Add files via upload

---
+++ xx.py
@@ -0,0 +1,22 @@
+import sys, time
+def jalan (kata):
+	for e in kata:
+	    sys.stdout.write(e)
+	    sys.stdout.flush()
+	    time.sleep(0.1)
+jalan('          >>>>>>>>>>os.system<<<<<<<<<<<               '.center(44))
+jalan('          >>>>>>>>>>TeaMCyber<<<<<<<<<<<                '.center(44))
+nama = input('Siapa Nama Anda?')
+jalan('          >>>>>>>Selamat Datang<<<<<<<<<               '.center(44))
+print (nama)
+jalan('          >>>>>>>>>> uji Ping <<<<<<<<<<<<<              '.center(44))
+import os
+os.system('clear')
+import os
+
+os.system ("ping 8.8.8.8")
+
+a = 0
+while (a< 3):
+	print ("NPC TUBE")
+	print ("🚩🚩🚩🚩🚩🚩")
